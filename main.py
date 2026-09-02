b0ee1b43524374580b979b744d424b49e04665f1
v <221338086+pigeonsdontcode@users.noreply.github.com>
2026-09-03 00:32:15 +0200

    feat: setup base tui logic

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
+from blessed import Terminal
 from app.data import readoptions
+from app.menu import pfx, Selection
 
 def main() -> None:
+    term = Terminal()
+
+    with \
+        (
+            term.fullscreen(),
+            term.cbreak(),
+            term.hidden_cursor()
+        ):
+
+        # menu logic starts here
+        while True:
+            print(term.clear)
+
+            test = [
+                i for i in range(10)
+            ]
+
+            for num in test:
+                print(num, flush=True)
+
+            # key input handling later
+            key = term.inkey()
+
+            # force buffer update to prevent screen flickering
+            for _ in range(10):
+                print(' ', flush=True)
+                # print(' ', end='', flush=True)
+
+
     print(readoptions())
 
 if __name__ == "__main__":
