3a83a628a4d0e59e76da499476f159076c4372cd
v <221338086+pigeonsdontcode@users.noreply.github.com>
2026-09-03 21:56:55 +0200

    feat: add key handling to menu

--- app/menu.py
+++ app/menu.py
@@ -1,4 +1,5 @@
 from builtins import print as _p
+from app.logic import changeselection, check, collectplanned
 
 # rewrite print functions to enable flush by default
 # to reduce output buffering and minimize tui flickering
@@ -15,22 +16,23 @@ class pfx:
         pre = uiprefix + spacing
 
         # use formatted strings to add colors later
-        base = f'{pre}[ ] '
+        base = f'{pre} [ ] '
         base_hover = f'{pre}>[ ] '
-        checked = f'{pre}[X] '
+        checked = f'{pre} [X] '
         checked_hover = f'{pre}>[X] '
 
     class child:
         pre = uiprefix + (spacing * 2)
 
-        base = f'{pre}[ ] '
+        base = f'{pre} [ ] '
         base_hover = f'{pre}>[ ] '
-        checked = f'{pre}[X] '
+        checked = f'{pre} [X] '
         checked_hover = f'{pre}>[X] '
 
 class Selection:
     def __init__(self) -> None:
-        self.selection = 0
+        self.default = 1
+        self.selection = self.default
 
     # just incase we might use int method (probably not)
     def __int__(self):
@@ -64,5 +66,35 @@ def fmt(
         (False, True): p.checked,
         (True, True): p.checked_hover,
     }
-    
+
     return pfxmap[(selected, checked)] + content
+
+def handlekey(
+        key,
+        selection: Selection,
+        increment: int,
+        arr: list,
+        planned: list
+        ) -> None:
+    # possible menu keybinds
+    binds = {
+        # arrow key up bind,
+        # will decrement eventual selection to move the arrow up
+        'KEY_UP': lambda: changeselection('up', selection, increment),
+        # arrow key down bind,
+        # will incremenet selection to move arrow down
+        'KEY_DOWN': lambda: changeselection('down', selection, increment),
+        # enter key bind,
+        # will accent and proceed with the selected functions
+        # (and exiting selection menu)
+        'KEY_ENTER': lambda: collectplanned(arr, planned),
+        # None for now defines space bar, but also binds to other keys
+        # due to lib tomfoolery, will likely need a replacement later
+        None: lambda: check(arr, selection.value)
+    }
+
+
+    action = binds.get(key)
+    # linter error wants me to handle None (the dict can't return None)
+    if action != None:
+        action()
