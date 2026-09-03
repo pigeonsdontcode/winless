3e0a16c74accf50bef95ad89af9bdf5ebd961d23
v <221338086+pigeonsdontcode@users.noreply.github.com>
2026-09-03 15:46:18 +0200

    feat: add hardcoded option parsing algorithm with base incrementation

--- app/data.py
+++ app/data.py
@@ -2,7 +2,7 @@ import json
 
 config_path = 'config/options.json'
 
-def readoptions() -> list:
+def fetchoptions() -> list:
     filedata = open(config_path, 'r')
     arr = json.loads(filedata.read())
 
--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 from blessed import Terminal
-from app.data import readoptions
-from app.menu import pfx, Selection
+from app.data import fetchoptions
+from app.menu import pfx, Selection, fmt
 
 def main() -> None:
     term = Terminal()
 
-    with \
-        (
-            term.fullscreen(),
-            term.cbreak(),
-            term.hidden_cursor()
+    sel = Selection()
+    options = fetchoptions()
+
+    with (
+        term.fullscreen(),
+        term.cbreak(),
+        term.hidden_cursor()
         ):
 
         # menu logic starts here
         while True:
             print(term.clear)
 
-            test = [
-                i for i in range(10)
-            ]
+            global i
+            i = 0
 
-            for num in test:
-                print(num, flush=True)
+            for parent in options:
+                i += 1
+
+                print(
+                    fmt(
+                        parent[1][0],
+                        sel == i,
+                        parent[1][3],
+                        True
+                    )
+                )
+                
+                for child in parent[1][2]:
+                    i += 1
+                    print(
+                        fmt(
+                            child[1][0],
+                            sel == i,
+                            child[2] or parent[1][3]
+                        )
+                    )
 
             # key input handling later
             key = term.inkey()
@@ -29,9 +49,6 @@ def main() -> None:
             # reduce buffer output to prevent screen flickering
             for _ in range(10):
                 print(' ', flush=True)
-
-
-    print(readoptions())
 
 if __name__ == "__main__":
     try:
