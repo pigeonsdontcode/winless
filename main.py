2a4f3333c45d835c962fbd959135c81d2ab7ab12
v <221338086+pigeonsdontcode@users.noreply.github.com>
2026-09-03 21:58:15 +0200

    feat: add menu changes to main

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from blessed import Terminal
 from app.data import fetchoptions
-from app.menu import pfx, Selection, fmt
+from app.menu import (
+    Selection,
+    fmt,
+    handlekey
+)
 
 def main() -> None:
     term = Terminal()
 
     sel = Selection()
     options = fetchoptions()
+
+    global planned
+    planned = []
 
     with (
         term.fullscreen(),
@@ -27,28 +34,34 @@ def main() -> None:
                 print(
                     fmt(
                         parent[1][0],
-                        sel == i,
+                        sel.value == i,
                         parent[1][3],
                         True
-                    )
+                    ),
+                    flush=True
                 )
-                
+
                 for child in parent[1][2]:
                     i += 1
                     print(
                         fmt(
                             child[1][0],
-                            sel == i,
+                            sel.value == i,
                             child[2] or parent[1][3]
-                        )
+                        ),
+                        flush=True
                     )
 
             # key input handling later
             key = term.inkey()
+            handlekey(key.name, sel, i, options, planned)
 
             # reduce buffer output to prevent screen flickering
             for _ in range(10):
                 print(' ', flush=True)
+
+    print("run!")
+    print(planned)
 
 if __name__ == "__main__":
     try:
