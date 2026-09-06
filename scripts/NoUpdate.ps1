32e6d3e61961f6b34a5b2aaded36ce12737fec4b
v <221338086+pigeonsdontcode@users.noreply.github.com>
2026-09-06 20:07:23 +0200

    feat: add PauseUpdates and DeleteCachedUpdates functionality to scripts

--- scripts/NoUpdate.ps1
+++ scripts/NoUpdate.ps1
@@ -26,15 +26,32 @@ function RemovePowerOptions() {
     # the ideal value would be 0101 (show shutdown + restart)
     # which equals to 5 in decimal format
 
-    # windows will gradually force the power options with updates
-    # please note that windows will check and perform updates in different ways,
+    # Windows will gradually force the power options with updates
+    # please note that Windows will check and perform updates in different ways,
     # disabling the power menu options alone is not enough to not update
 }
 
 function PauseUpdates() {
+    # Windows update settings only allows a limited datetime set as paused expiration date
+    # this is easily bypassed by changing the according registry value to any date we want
 
+    # a date set to year 3001 or higher will return an error on the settings's update page
+    # please note that while updates may seem to not be working,
+    # Windows will just not read the invalid expiry time and continue to download and enforce updates regardless
+    # hence why we set it to a safer datetime in the near future, when Windows will have stopped being of any relevance
+    $PauseUpdatesPath = "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings"
+    $PauseUpdatesKey = "PauseUpdatesExpiryTime"
+
+    $dt = [datetime]::new(
+        2100, # years
+        12, # months
+        31, # days
+        0, 0, 0, # hms
+    ).toString("yyyy-MM-ddTHH:mm:ssZ") # converted to registry string datetime format
+
+    Set-ItemProperty -Path $PauseUpdatesPath -Name $PauseUpdatesKey -Value $dt -Type String
 }
 
 function DeleteCachedUpdates() {
-
+    Remove-Item -Path "$Env:SystemRoot\SoftwareDistribution\*" -Recurse -Force
 }
