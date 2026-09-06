0cd846e1954f831644287948a6488dcb8d76efbc
v <221338086+pigeonsdontcode@users.noreply.github.com>
2026-09-06 17:36:14 +0200

    feat: add RemovePowerOptions to update feature

--- scripts/NoUpdate.ps1
+++ scripts/NoUpdate.ps1
@@ -11,3 +11,30 @@ function DisableUpdateServices() {
         Stop-Service -Verbose -Name $svc -Force
     }
 }
+
+function RemovePowerOptions() {
+    $PowerMenuRegPath = "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\Orchestrator"
+    $PowerMenuRegKey = "ShutdownFlyoutOptions"
+
+    Set-ItemProperty -Path $PowerMenuRegPath -Name $PowerMenuRegKey -Value 5 -Type DWORD
+    # expected value is a 4-bit binary table / 0000
+    # 0 first digit = update & shutdown (this is always a lie, it reboots after update)
+    # 0 second digit = shutdown
+    # 0 third digit = update & reboot
+    # 0 fourth digit = restart
+
+    # the ideal value would be 0101 (show shutdown + restart)
+    # which equals to 5 in decimal format
+
+    # windows will gradually force the power options with updates
+    # please note that windows will check and perform updates in different ways,
+    # disabling the power menu options alone is not enough to not update
+}
+
+function PauseUpdates() {
+
+}
+
+function DeleteCachedUpdates() {
+
+}
