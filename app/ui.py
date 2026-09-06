4a4fa976b223e023b697953e01aba6fee42be1b3
v <221338086+pigeonsdontcode@users.noreply.github.com>
2026-09-06 17:26:36 +0200

    fix: remove unnecessary loop

--- app/ui.py
+++ app/ui.py
@@ -114,43 +114,42 @@ def run_tasks(
         term.hidden_cursor()
         ):
 
-        while True:
-            total = 0
+        total = 0
 
-            # find total task count to display
-            for task in tasks:
-                for _ in task[1]:
-                    total += 1
+        # find total task count to display
+        for task in tasks:
+            for _ in task[1]:
+                total += 1
 
-            print(f"\n{clr.blue}{total}{clr.r} task{'s' if total != 1 else ''} found", flush=True)
+        print(f"\n{clr.blue}{total}{clr.r} task{'s' if total != 1 else ''} found", flush=True)
 
-            current = 0
-            failed = 0
+        current = 0
+        failed = 0
 
-            for task in tasks:
-                name, script = task[0]
-                print(f"\nRunning: {name}", flush=True)
-                for option in task[1]:
-                    current += 1
-                    last = clr.gray if current != total else clr.white
+        for task in tasks:
+            name, script = task[0]
+            print(f"\nRunning: {name}", flush=True)
+            for option in task[1]:
+                current += 1
+                last = clr.gray if current != total else clr.white
 
-                    prefix = f" {last}{current}{clr.r}/{last}{total}{clr.r} "
-                    prefixerr = f" {clr.red}{current}{clr.r}/{last}{total}{clr.r} "
+                prefix = f" {last}{current}{clr.r}/{last}{total}{clr.r} "
+                prefixerr = f" {clr.red}{current}{clr.r}/{last}{total}{clr.r} "
 
-                    func, msg = option
-                    print(f"{prefix}{msg}", flush=True)
+                func, msg = option
+                print(f"{prefix}{msg}", flush=True)
 
-                    # powershell handler is the only part of the codebase with proper error handling
-                    # but we still don't utilize Exceptions!
-                    try:
-                        run(path, script, func)
-                    except Exception:
-                        print(f"{prefixerr}{clr.red}An Error occurred unexpectedly")
-                        failed += 1
+                # powershell handler is the only part of the codebase with proper error handling
+                # but we still don't utilize Exceptions!
+                try:
+                    run(path, script, func)
+                except Exception:
+                    print(f"{prefixerr}{clr.red}An Error occurred unexpectedly")
+                    failed += 1
 
-                    time.sleep(0.2)
+                time.sleep(0.2)
 
-            return (
-                total,
-                failed
-            )
+        return (
+            total,
+            failed
+        )
